e4212356806348dea5e1d65beb881b8e3c098c5e
Willie Wheeler <willie.wheeler@gmail.com>
2017-10-15 00:22:31 -0700

    cleanup

--- src/main/java/com/williewheeler/weatherreport/controller/WeatherController.java
+++ src/main/java/com/williewheeler/weatherreport/controller/WeatherController.java
@@ -1,22 +1,23 @@
 package com.williewheeler.weatherreport.controller;
 
-import com.williewheeler.weatherreport.domain.template.binding.WeatherReport;
 import com.williewheeler.weatherreport.domain.service.WeatherService;
+import com.williewheeler.weatherreport.domain.template.binding.WeatherReport;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController("/weather")
+@CrossOrigin
 public class WeatherController {
 
 	@Autowired
 	private WeatherService weatherService;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public List<WeatherReport> getWeatherReports(@RequestParam(value = "resilient", defaultValue = "true") boolean resilient) {
 		return weatherService.getWeatherReports(resilient);
 	}
