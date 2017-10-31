64ac1763fcf262e78df22ff0fd536f8cad2f7e68
Willie Wheeler <wwheeler@expedia.com>
2017-10-31 10:25:08 +0000

    Updated hardcoded city

--- src/main/java/com/williewheeler/weatherreport/domain/store/CityStoreHardcoded.java
+++ src/main/java/com/williewheeler/weatherreport/domain/store/CityStoreHardcoded.java
@@ -20,7 +20,8 @@ public class CityStoreHardcoded implements CityStore {
 						.id(0L)
 						.name("Bellevue, WA, US")
 						.owmCityId(5786882L)
-						.gistUri("https://gist.githubusercontent.com/williewheeler/151d6c2104e9884ed112225cb3620344/raw/1b6539c530367a285ffd56f6d59754bd42fd20d6/openweathermap-bellevue.json")
+						.gistUri("https://cdn.rawgit.com/williewheeler/151d6c2104e9884ed112225cb3620344/raw/1b6539c530367a285ffd56f6d59754bd42fd20d6/openweathermap-bellevue.json")
+						.s3Uri("https://s3.amazonaws.com/willie/public/weatherreport/openweathermap-bellevue.json")
 						.build()
 		);
 	}
