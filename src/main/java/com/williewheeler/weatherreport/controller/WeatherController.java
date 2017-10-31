1ffdd481bd9782f6f0a0940959f66665056117a5
Willie Wheeler <wwheeler@expedia.com>
2017-10-31 08:52:12 +0000

    Added resilience trace

--- src/main/java/com/williewheeler/weatherreport/controller/WeatherController.java
+++ src/main/java/com/williewheeler/weatherreport/controller/WeatherController.java
@@ -2,6 +2,8 @@ package com.williewheeler.weatherreport.controller;
 
 import com.williewheeler.weatherreport.connector.openweathermap.WeatherReport;
 import com.williewheeler.weatherreport.domain.service.WeatherService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +16,7 @@ import java.util.List;
 @RestController("/weather")
 @CrossOrigin
 public class WeatherController {
+	private static final Logger LOG = LoggerFactory.getLogger(WeatherController.class);
 	
 	@Autowired
 	@Qualifier("weatherServiceResilient")
@@ -25,6 +28,7 @@ public class WeatherController {
 
 	@GetMapping
 	public List<WeatherReport> getWeatherReports(@RequestParam(value = "resilient", defaultValue = "true") boolean resilient) {
+		LOG.trace("Getting weather reports: resilient={}", Boolean.toString(resilient));
 		return resilient ?
 				weatherServiceResilient.getWeatherReports() :
 				weatherServiceNonResilient.getWeatherReports();
